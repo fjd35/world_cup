0c6b84e17a26a69dca229fd99650e285c5c883e6
Fergal Duggan <fjd35@cam.ac.uk>
2022-11-22 23:10:03 +0000

    Revert "Refactor to try and remove circular import"
    This reverts commit dbee53a9148cdbdb265fe77a4dbf2809a9dd8d4d.

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+from .models import *
+
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "AnActualSecretKey"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../db.sqlite"
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
 
-from .db import db
+db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     __tablename__ = "user"
