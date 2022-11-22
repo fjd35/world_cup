c45605fc03a2910903353ac37dcb3080abcbaed8
Fergal Duggan <fjd35@cam.ac.uk>
2022-11-22 08:25:26 +0000

    Basic web layout

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+import pandas as pd
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 
 from . import db
@@ -8,10 +9,52 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
-    matches = db.session.query(Match).order_by(Match.id).all()
-    return render_template("index.html", matches=matches)
+    df = get_summary_df()
+    return render_template("index.html", df=df)
 
-@main.route("/profile")
+@main.route("/my_predictions")
 @login_required
-def profile():
-    return render_template("profile.html", name=current_user.name)
+def my_predictions():
+    df = get_predictions_df()
+    return render_template("my_predictions.html", user=current_user, df=df)
+
+@main.route("/add_prediction", methods=["POST"])
+def add_prediction(match_id: int, score1: int, score2: int):
+    new_prediction = Prediction(user_id=current_user.id, match_id=match_id, score1=score1, score2=score2)
+    db.session.add(new_prediction)
+    db.session.commit()
+    return redirect(url_for("main.my_predictions"))
+
+def get_summary_df() -> pd.DataFrame:
+    matches = db.session.query(Match).filter(Match.is_finished == True).order_by(Match.id).all()
+    users = db.session.query(User).order_by(User.id).all()
+    columns = ["Team 1", "Score", "Team 2"] + [user.username for user in users]
+    data = []
+    for match in matches:
+        row = [match.team1, (match.score1, match.score2), match.team2]
+        for user in users:
+            try:
+                prediction = next(filter(lambda p: p.match.id == match.id, user.predictions))
+            except StopIteration:
+                row += [(None, None)]
+            else:
+                row += [(prediction.score1, prediction.score2)]
+        data.append(row)
+    df = pd.DataFrame(data, columns=columns)
+    return df
+
+def get_predictions_df() -> pd.DataFrame:
+    matches = db.session.query(Match).order_by(Match.id).all()
+    columns = ["Team 1", "Your prediction", "Team 2", "Editable"]
+    data = []
+    for match in matches:
+        try:
+            prediction = next(filter(lambda p: p.match.id == match.id, current_user.predictions))
+        except StopIteration:
+            prediction_score = (None, None)
+        else:
+            prediction_score = (prediction.score1, prediction.score2)
+        row = [match.team1, prediction_score, match.team2, not bool(match.is_finished)]
+        data.append(row)
+    df = pd.DataFrame(data, columns=columns)
+    return df
--- src/models.py
+++ src/models.py
@@ -8,23 +8,26 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    score = db.Column(db.Integer, nullable=False)
+    score = db.Column(db.Integer, nullable=False, default=0)
 
     predictions = db.relationship("Prediction", back_populates="user")
+
+    def __repr__(self):
+        return f"<User {self.id}: {self.username} {self.score}>"
 
 class Match(db.Model):
     __tablename__ = "match"
     id = db.Column(db.Integer, primary_key=True)
     team1 = db.Column(db.String(100), nullable=False)
     team2 = db.Column(db.String(100), nullable=False)
-    is_finished = db.Column(db.Boolean, nullable=False)
+    is_finished = db.Column(db.Boolean, nullable=False, default=False)
     score1 = db.Column(db.Integer, nullable=True)
     score2 = db.Column(db.Integer, nullable=True)
 
     predictions = db.relationship("Prediction", back_populates="match")
 
     def __repr__(self):
-        return f"<Match {self.id}>: {self.team1} {self.score1}-{self.score2} {self.team2}"
+        return f"<Match {self.id}: {self.team1} {self.score1}-{self.score2} {self.team2}>"
 
 class Prediction(db.Model):
     __tablename__ = "prediction"
@@ -36,4 +39,7 @@ class Prediction(db.Model):
 
     user = db.relationship("User", back_populates="predictions")
     match = db.relationship("Match", back_populates="predictions")
+
+    def __repr__(self):
+        return f"<Prediction {self.id}: ({self.user.username}) {self.match.team1} {self.score1}-{self.score2} {self.match.team2}>"
 
